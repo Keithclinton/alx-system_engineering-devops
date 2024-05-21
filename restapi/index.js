76c49f95b4b471dd551b6e043cafa5c324b17e0d
Keithclinton <keithnyaburi@gmail.com>
2024-05-21 07:27:13 -0700

    index.js

---
+++ restapi/index.js
@@ -0,0 +1,17 @@
+// simple Express.js RESTFul API //
+	'use strict';
+//initialize
+	const
+		port = 8888,
+		express = require('express'),
+		app = express();
+// /hello/ GET request
+	app.get('/hello/:name?, (req, res) => 
+		res.json(
+			{ message: `Hello ${req.params.name || 'world'}!` }
+		)
+	);
+// start server
+app.listen(port, () =>
+	console.log(``Server started on port ${port}`);
+	);
